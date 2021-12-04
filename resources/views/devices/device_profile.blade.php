b88c5062e122aa21c794377641b9815c4f30450e
nualiyu <mualiyuoox@gmaill.com>
2021-12-04 10:19:31 +0100

    Ats Version 1.01.1 on 4-12-2011 (Production)

--- resources/views/devices/device_profile.blade.php
+++ resources/views/devices/device_profile.blade.php
@@ -105,14 +105,14 @@
                               </div>
                             </div>
                             {{-- <hr> --}}
-                            {{-- <div class="row">
+                            <div class="row">
                               <div class="col-sm-3">
-                                <h6 class="mb-0" style="float: right;">Location</h6>
+                                <h6 class="mb-0" style="float: right;">Local Gov Area</h6>
                               </div>
                               <div class="col-sm-9 text-secondary">
-                                {{$device->location}}
+                                {{$device->lga}}
                               </div>
-                            </div> --}}
+                            </div>
                             {{-- <hr> --}}
                             <div class="row">
                               <div class="col-sm-3">
