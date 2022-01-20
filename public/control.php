c8de9d1a090177046098a11bc1caad8570ad2c6f
nualiyu <mualiyuoox@gmaill.com>
2022-01-20 06:45:15 +0100

    Control.php is added to update transactions

--- public/control.php
+++ public/control.php
@@ -44,6 +44,33 @@ if (isset($_POST['update_t'])) {
 			var pass1 = prompt('Access Denied - Password Incorrect, Please Try Again.', 'Password');
 		}
 		if (pass1.toLowerCase() != "password" & testV == 3) history.go(-1);
+
+
+		// Diseble inspect
+		document.addEventListener('keydown', function() {
+			if (event.keyCode == 123) {
+				alert("You Can not Do This!");
+				return false;
+			} else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+				alert("You Can not Do This!");
+				return false;
+			} else if (event.ctrlKey && event.keyCode == 85) {
+				alert("You Can not Do This!");
+				return false;
+			}
+		}, false);
+
+		if (document.addEventListener) {
+			document.addEventListener('contextmenu', function(e) {
+				alert("You Can not Do This A!");
+				e.preventDefault();
+			}, false);
+		} else {
+			document.attachEvent('oncontextmenu', function() {
+				alert("You Can not Do This B!");
+				window.event.returnValue = false;
+			});
+		}
 	</script>
 	<style>
 		html,
