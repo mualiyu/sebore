d92717802c2c9c5902163a5765c2e19522fe22a2
mualiyu <mualiyuoox@gmail.com>
2022-06-24 22:08:52 +0100

    control update

--- resources/views/layouts/Aindex.blade.php
+++ resources/views/layouts/Aindex.blade.php
@@ -99,19 +99,19 @@
     <meta name="keywords" content="Ticket, ticketing, system">
     <meta name="author" content="Ajisaq" />
     <!-- Favicon icon -->
-    <link rel="icon" href="{{asset('assets/images/logo.png')}}" type="image/x-icon">
+    <link rel="icon" href="{{url('assets/images/logo.png')}}" type="image/x-icon">
     <!-- Google font-->     <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
     <!-- Required Fremwork -->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{url('assets/css/bootstrap/css/bootstrap.min.css')}}">
     <!-- waves.css -->
-    <link rel="stylesheet" href="{{asset('assets/pages/waves/css/waves.min.css" type="text/css')}}" media="all">
+    <link rel="stylesheet" href="{{url('assets/pages/waves/css/waves.min.css" type="text/css')}}" media="all">
     <!-- themify-icons line icon -->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/icon/themify-icons/themify-icons.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{url('assets/icon/themify-icons/themify-icons.css')}}">
     <!-- Font Awesome -->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/icon/font-awesome/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{url('assets/icon/font-awesome/css/font-awesome.min.css')}}">
     <!-- Style.css -->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/jquery.mCustomScrollbar.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{url('assets/css/style.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{url('assets/css/jquery.mCustomScrollbar.css')}}">
 
     @yield('style')
 
@@ -284,7 +284,7 @@ if ($organization->logo) {
                         </div>
                     </div>
                     <a href="#">
-                        {{-- <img class="img-fluid" src="{{asset('assets/images/logo.png')}}" alt="Theme-Logo" /> --}}
+                        {{-- <img class="img-fluid" src="{{url('assets/images/logo.png')}}" alt="Theme-Logo" /> --}}
                         <a class="img-fluid">{{$organization->name}}</a>
                     </a>
                     <a class="mobile-options waves-effect waves-light">
@@ -316,7 +316,7 @@ if ($organization->logo) {
 
                         <li class="user-profile header-notification">
                             <a href="#!" class="waves-effect waves-light">
-                                <img src="{{asset('storage/pic/'.$pic)}}" class="img-radius" alt="User-Profile-Image">
+                                <img src="{{url('storage/pic/'.$pic)}}" class="img-radius" alt="User-Profile-Image">
                                 <span>{{$agent->name}}</span>
                                 <i class="ti-angle-down"></i>
                             </a>
@@ -353,7 +353,7 @@ if ($organization->logo) {
                     <div class="pcoded-inner-navbar main-menu">
                         <div class="">
                             {{-- <div class="main-menu-header">
-                                <img class="img-80 img-radius" src="{{asset('storage/pic/'.$pic)}}" alt="User-Profile-Image">
+                                <img class="img-80 img-radius" src="{{url('storage/pic/'.$pic)}}" alt="User-Profile-Image">
                                 <div class="user-details">
                                     <span id="more-details">{{Auth::user()->name}}<i class="fa fa-caret-down"></i></span>
                                 </div>
@@ -443,20 +443,20 @@ if ($organization->logo) {
 </div>
 
 <!-- Required Jquery -->
-    <script type="text/javascript" src="{{asset('assets/js/jquery/jquery.min.js')}}"></script>     <script type="text/javascript" src="assets/js/jquery-ui/jquery-ui.min.js "></script>     <script type="text/javascript" src="assets/js/popper.js/popper.min.js"></script>     
+    <script type="text/javascript" src="{{url('assets/js/jquery/jquery.min.js')}}"></script>     <script type="text/javascript" src="assets/js/jquery-ui/jquery-ui.min.js "></script>     <script type="text/javascript" src="assets/js/popper.js/popper.min.js"></script>     
     <script type="text/javascript" src="assets/js/bootstrap/js/bootstrap.min.js "></script>
 <!-- waves js -->
-<script src="{{asset('assets/pages/waves/js/waves.min.js')}}"></script>
+<script src="{{url('assets/pages/waves/js/waves.min.js')}}"></script>
 <!-- jquery slimscroll js -->
-<script type="text/javascript" src="{{asset('assets/js/jquery-slimscroll/jquery.slimscroll.js')}}"></script>
+<script type="text/javascript" src="{{url('assets/js/jquery-slimscroll/jquery.slimscroll.js')}}"></script>
 <!-- modernizr js -->
-    <script type="text/javascript" src="{{asset('assets/js/SmoothScroll.js')}}"></script>     
-    <script src="{{asset('assets/js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
-<script src="{{asset('assets/js/pcoded.min.js')}}"></script>
-<script src="{{asset('assets/js/vertical-layout.min.js')}}"></script>
-<script src="{{asset('assets/js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+    <script type="text/javascript" src="{{url('assets/js/SmoothScroll.js')}}"></script>     
+    <script src="{{url('assets/js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+<script src="{{url('assets/js/pcoded.min.js')}}"></script>
+<script src="{{url('assets/js/vertical-layout.min.js')}}"></script>
+<script src="{{url('assets/js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
 <!-- Custom js -->
-<script type="text/javascript" src="{{asset('assets/js/script.js')}}"></script>
+<script type="text/javascript" src="{{url('assets/js/script.js')}}"></script>
 @yield('script')
 
 <script>
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -77,21 +77,21 @@
     <meta name="author" content="Ajisaq" />
       <!-- Favicon icon -->
 
-      <link rel="icon" href="{{asset('assets/images/logo.png" type="image/x-icon')}}">
+      <link rel="icon" href="{{url('assets/images/logo.png" type="image/x-icon')}}">
       <!-- Google font-->     
       <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
       <!-- Required Fremwork -->
-      <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap/css/bootstrap.min.css')}}">
+      <link rel="stylesheet" type="text/css" href="{{url('assets/css/bootstrap/css/bootstrap.min.css')}}">
       <!-- waves.css -->
-      <link rel="stylesheet" href="{{asset('assets/pages/waves/css/waves.min.css')}}" type="text/css" media="all">
+      <link rel="stylesheet" href="{{url('assets/pages/waves/css/waves.min.css')}}" type="text/css" media="all">
       <!-- themify-icons line icon -->
-      <link rel="stylesheet" type="text/css" href="{{asset('assets/icon/themify-icons/themify-icons.css')}}">
+      <link rel="stylesheet" type="text/css" href="{{url('assets/icon/themify-icons/themify-icons.css')}}">
       <!-- ico font -->
-      <link rel="stylesheet" type="text/css" href="{{asset('assets/icon/icofont/css/icofont.css')}}">
+      <link rel="stylesheet" type="text/css" href="{{url('assets/icon/icofont/css/icofont.css')}}">
       <!-- Font Awesome -->
-      <link rel="stylesheet" type="text/css" href="{{asset('assets/icon/font-awesome/css/font-awesome.min.css')}}">
+      <link rel="stylesheet" type="text/css" href="{{url('assets/icon/font-awesome/css/font-awesome.min.css')}}">
       <!-- Style.css -->
-      <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
+      <link rel="stylesheet" type="text/css" href="{{url('assets/css/style.css')}}">
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
 
       @yield('style')
@@ -210,23 +210,23 @@
 
 
 <!-- Required Jquery -->
-    <script type="text/javascript" src="{{asset('assets/js/jquery/jquery.min.js')}}"></script>     
+    <script type="text/javascript" src="{{url('assets/js/jquery/jquery.min.js')}}"></script>     
     <script type="text/javascript" src="assets/js/jquery-ui/jquery-ui.min.js "></script>     
     <script type="text/javascript" src="assets/js/popper.js/popper.min.js"></script>     
     <script type="text/javascript" src="assets/js/bootstrap/js/bootstrap.min.js "></script>
 <!-- waves js -->
-<script src="{{asset('assets/pages/waves/js/waves.min.js')}}"></script>
+<script src="{{url('assets/pages/waves/js/waves.min.js')}}"></script>
 <!-- jquery slimscroll js -->
-<script type="text/javascript" src="{{asset('assets/js/jquery-slimscroll/jquery.slimscroll.js')}}"></script>
+<script type="text/javascript" src="{{url('assets/js/jquery-slimscroll/jquery.slimscroll.js')}}"></script>
 <!-- modernizr js -->
-    <script type="{{asset('text/javascript" src="assets/js/SmoothScroll.js')}}"></script>     
+    <script type="{{url('text/javascript" src="assets/js/SmoothScroll.js')}}"></script>     
     <script src="assets/js/jquery.mCustomScrollbar.concat.min.js "></script>
 <!-- i18next.min.js -->
 <script type="text/javascript" src="bower_components/i18next/js/i18next.min.js"></script>
 <script type="text/javascript" src="bower_components/i18next-xhr-backend/js/i18nextXHRBackend.min.js"></script>
 <script type="text/javascript" src="bower_components/i18next-browser-languagedetector/js/i18nextBrowserLanguageDetector.min.js"></script>
 <script type="text/javascript" src="bower_components/jquery-i18next/js/jquery-i18next.min.js"></script>
-<script type="text/javascript" src="{{asset('assets/js/common-pages.js')}}"></script>
+<script type="text/javascript" src="{{url('assets/js/common-pages.js')}}"></script>
 
 <script>
     const togglePassword = document.querySelector('#togglePassword');
